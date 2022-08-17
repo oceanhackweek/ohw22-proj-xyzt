da6c6b74051af64b6c1531ebdfb72d0ced0f67d3
Johnathan Evanilla <jevanilla@bigelow.org>
2022-08-17 10:41:36 -0400

    Adding copernicus package skeleton

---
+++ copernicus/R/copernicus-package.R
@@ -0,0 +1,7 @@
+#' copernicus: Simplified access to copernicus data from R.
+#'
+#' @description Simplified access to copernicus data from R.
+#' @docType package
+#' @name copernicus
+#' @importFrom rlang .data := !! set_names
+NULL
