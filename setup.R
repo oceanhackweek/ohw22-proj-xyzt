34590d3b5c67ae752b4336422ed8a518f0a749d6
Ben Tupper <btupper@bigelow.org>
2022-08-18 08:36:48 -0400

    drop "_m" from depth in msfd, add autmatic sourcing of functions

--- setup.R
+++ setup.R
@@ -1,5 +1,6 @@
 # Use this for the development cycle to load all necessary packages
 
+PATH <- "."
 
 suppressPackageStartupMessages({
   required_cran <- c("dplyr", "readr", "stars", "sf", "devtools", "leaflet")
@@ -16,6 +17,16 @@ suppressPackageStartupMessages({
   ok <- sapply(required_cran, library, character.only = TRUE)
   ok <- sapply(required_github, library, character.only = TRUE)
 })
+
+#' Source all of the functions found in `script`
+#' @param path the path to the files by default assumes "[current_working_directory]/script"
+#' @return invisible NULL
+source_files <- function(path = file.path(PATH, "script")){
+  ff <- list.files(path, pattern = glob2rx("*.R"), full.names = TRUE)
+  for (f in ff) source(f)
+  invisible(NULL)
+}
+
 
 
 
